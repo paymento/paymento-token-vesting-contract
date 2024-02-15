bb8f2d7aba4c921c63b16c09e4005bc36d871897
Hadi <1774020+hadideveloper@users.noreply.github.com>
2024-02-15 15:21:11 +0800

    Test token allocate

--- test/vesting-buy-claim.js
+++ test/vesting-buy-claim.js
@@ -81,7 +81,7 @@ contract('MockedVesting', async () => {
     it('Check the amount of token user can claim', async () => {
         const claimableTokens = await vestingContract.checkClaimableTokens(2, testAccount1);
 
-        //in stage 2, vesting days is 480 days
+        
         // so, 9200/480 = 19.166666666666
         assert.equal(claimableTokens, 19166666666666666666);
     });
